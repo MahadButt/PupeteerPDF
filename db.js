0dac488f3b1fb3c4c63ac292f4248687d20dd501
MahadButt <59567070+MahadButt@users.noreply.github.com>
2022-08-21 08:03:32 +0500

    Add files via upload
    update pupeteer code

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+var dbURI = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+mongoose.connect(dbURI, { useNewUrlParser: true ,useUnifiedTopology: true});
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to MongoDB');
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+require('./models/TokenAuthentication');
+require('./models/FileStructure');
